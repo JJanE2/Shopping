3669f59c581d36f8f2800f28e5f45815c3676e1a
JJanE2 <aaa1685@naver.com>
2025-12-03 14:49:39 +0900

    chore: set initial stockQuantity values in data-oracle.sql

--- src/main/resources/data-oracle.sql
+++ src/main/resources/data-oracle.sql
@@ -4,8 +4,8 @@ INSERT INTO member (id, loginId, nickname, password, role)
 VALUES (member_seq.NEXTVAL, 'q', 'test', 'q', 'OWNER')/
 
 -- Product data
-INSERT INTO product (id, memberId, name, price, description)
-VALUES (product_seq.NEXTVAL, 1, 'testProduct', 1000, 'testDesc')/
+INSERT INTO product (id, memberId, name, price, description, stockQuantity)
+VALUES (product_seq.NEXTVAL, 1, 'testProduct', 1000, 'testDesc', 50)/
 
 -- Order data
 INSERT INTO orders (id, memberId, totalPrice)
