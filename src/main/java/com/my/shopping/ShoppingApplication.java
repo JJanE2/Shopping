0b86beb61a6499276509bff070056d8b36e52690
JJanE2 <aaa1685@naver.com>
2026-01-04 14:30:35 +0900

    chore: add deployment configuration

--- src/main/java/com/my/shopping/ShoppingApplication.java
+++ src/main/java/com/my/shopping/ShoppingApplication.java
@@ -2,12 +2,19 @@ package com.my.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShoppingApplication {
+public class ShoppingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingApplication.class, args);
 	}
 
+	// for Deploy
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ShoppingApplication.class);
+	}
 }
